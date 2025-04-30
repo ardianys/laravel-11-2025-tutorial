af12f45ab1e841d45dda25155c6b3549f4800a13
ardianys <ardianys@gmail.com>
2025-04-30 10:24:43 +0700

    various update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,7 +33,9 @@
     <link rel="manifest" href="/site.webmanifest">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
-    <link href="./output.css" rel="stylesheet">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <script async defer src="https://buttons.github.io/buttons.js"></script>
 </head>
 <body>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,8 +17,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::resource('posts', PostController::class)->only(['index', 'show']);
+
 Route::middleware('auth')->group(function () {
-    Route::resource('/posts', \App\Http\Controllers\PostController::class);
+
+    Route::resource('posts', PostController::class)->except(['index', 'show']);
+    // Route::resource('/posts', \App\Http\Controllers\PostController::class);
     Route::post('posts/{post}/comments', [\App\Http\Controllers\CommentController::class, 'store'])->name('comments.store');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
